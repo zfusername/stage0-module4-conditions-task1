c9a9a48f7d8c73fff8894d5e870d83d8db0defad
zfusername <farrukh_ziyaev@epam.com>
2023-07-20 16:06:01 +0500

    change

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if(true){
+            System.out.println(true);
+        }
     }
 }
